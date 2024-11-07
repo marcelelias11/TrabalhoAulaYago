f46f74395c45a227358f2f46f68c9f492fbc1a99
Marcelelias11 <marcelelias11@gmail.com>
2024-11-07 18:06:13 -0300

    First Commit

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import BasicExample from './components/box'
+import './App.css'
+import * as amplitude from '@amplitude/analytics-browser';
+
+function App() {
+  if (localStorage.getItem("hasVoted") == undefined ){
+    localStorage.setItem("hasVoted", "0")
+  }
+  amplitude.init('c46a7699d239e619ba942793f5e6f7cd', {"autocapture":true});
+  const [count, setCount] = useState(0)
+  const [options, setOptions] = useState([]);
+  
+  function sendResult() {
+    if (
+      localStorage.getItem("hasVoted") === "0" ||
+      localStorage.getItem("hasVoted") == undefined
+    ) {
+      fetch("http://localhost:8080/aula", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          opcao1: document.getElementById("empresas").value,
+          opcao2: document.getElementById("candidatos").value,
+        }),
+      })
+        .then(async function (response) {
+          localStorage.setItem("hasVoted", "1");
+          console.log(response);
+          return await response.text();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      alert("Você já votou!");
+    }
+  }
+
+  function resultado() {
+    fetch("http://localhost:8080/votos", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async function (response) {
+        console.log(response);
+        return await response.json();
+      }).then(async function (text) {
+        console.log(text)
+        let i = 0
+        let alerttext = ""
+        for (i in text.opcoes) {
+          alerttext += `${text.opcoes[i]}: ${text.votos[i]} votos \n`
+        }
+        alert(alerttext)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <>
+      <div id="cartascontainer">
+      <form>
+        <label>Empresa:</label>
+        <select name="empresas" id="empresas">
+          <option>Opção 1</option>
+          <option>Opção 2</option>
+          </select>
+        <br /><br />
+        <label>Candidato:</label>
+        <select name="candidatos" id="candidatos">
+        <option>Opção 3</option>
+        <option>Opção 4</option></select>
+        <br /><br />
+      </form>
+      <div id="buttondiv">
+        <button id="formsubmit" onClick={sendResult}>Enviar</button>
+        <button id="mostrarvotos" onClick={resultado}>Mostrar Votos</button>
+      </div>
+      <div id="tablediv"><table id="senttable"></table></div>
+    </div>
+    </>
+  )
+}
+
+export default App
